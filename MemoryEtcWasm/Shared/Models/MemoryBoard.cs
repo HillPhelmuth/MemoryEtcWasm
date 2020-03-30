dbe79571b7070c5b6cbc53b7bba61bc7ba5d85c2
HillPhelmuth <46843652+HillPhelmuth@users.noreply.github.com>
2020-03-29 19:12:23 -0500

    Add difficulty levels

--- MemoryEtcWasm/Shared/Models/MemoryBoard.cs
+++ MemoryEtcWasm/Shared/Models/MemoryBoard.cs
@@ -7,51 +7,114 @@ namespace MemoryEtcWasm.Shared.Models
     public class MemoryBoard
     {
         public MemoryCard[] Board { get; set; }
-        
-        public MemoryBoard()
+
+
+        public MemoryBoard(string difficulty)
         {
-            string[] pairs = new string[] {
-                @"images/babypic.jpg",
-                @"images/babypic.jpg",
-                @"images/blue-angels.jpg",
-                @"images/blue-angels.jpg",
-                @"images/FancyBroodGum.jpg",
-                @"images/FancyBroodGum.jpg",
-                @"images/giraffe.jpg",
-                @"images/giraffe.jpg",
-                @"images/GirlAtGym.jpg",
-                @"images/GirlAtGym.jpg",
-                @"images/PipeSmoker.jpg",
-                @"images/PipeSmoker.jpg",
-                @"images/pragueNights.jpg",
-                @"images/pragueNights.jpg",
-                @"images/UglyFlower.jpg",
-                @"images/UglyFlower.jpg"
-            };
-            //string[] pairs = new string[] {
-            //    @"images/ChaseCrawford.jpg",
-            //    @"images/ChaseCrawford.jpg",
-            //    @"images/IamSomerhalder.jpg",
-            //    @"images/IamSomerhalder.jpg",
-            //    @"images/KaiaGerber.jpg",
-            //    @"images/KaiaGerber.jpg",
-            //    @"images/KateMoss.jpg",
-            //    @"images/KateMoss.jpg",
-            //    @"images/LucasHood.jpg",
-            //    @"images/LucasHood.jpg",
-            //    @"images/MaggieQ.jpg",
-            //    @"images/MaggieQ.jpg",
-            //    @"images/MamieGummer.jpg",
-            //    @"images/MamieGummer.jpg",
-            //    @"images/PrinceHarry.jpg",
-            //    @"images/PrinceHarry.jpg"
-            //};
-            Board = new MemoryCard[16];
-            for (int i = 0; i < 16; i++)
+            Console.WriteLine("Difficulty set to: " + difficulty);
+            switch (difficulty)
             {
-                Board[i] = new MemoryCard(pairs[i]);
+                case "medium":
+                    Pairs = mediumPairs;
+                    break;
+                case "hard":
+                    Pairs = hardPairs;
+                    break;
+                default:
+                    Pairs = easyPairs;
+                    break;
+            }
+            var length = Pairs.Length;
+            Console.WriteLine($"no. of cards (model class): {length}");
+            Board = new MemoryCard[length];
+            for (int i = 0; i < length; i++)
+            {
+                Board[i] = new MemoryCard(Pairs[i]);
+                Console.WriteLine($"created card {i + 1}");
             }
         }
+        private string[] Pairs { get; set; }
 
+        private static readonly string[] easyPairs = new string[] {
+            @"images/babypic.jpg",
+            @"images/babypic.jpg",
+            @"images/blue-angels.jpg",
+            @"images/blue-angels.jpg",
+            @"images/FancyBroodGum.jpg",
+            @"images/FancyBroodGum.jpg",
+            @"images/giraffe.jpg",
+            @"images/giraffe.jpg",
+            @"images/GirlAtGym.jpg",
+            @"images/GirlAtGym.jpg",
+            @"images/PipeSmoker.jpg",
+            @"images/PipeSmoker.jpg",
+            @"images/pragueNights.jpg",
+            @"images/pragueNights.jpg",
+            @"images/UglyFlower.jpg",
+            @"images/UglyFlower.jpg"
+        };
+        private static readonly string[] mediumPairs = new string[]
+        {
+            @"images/babypic.jpg",
+            @"images/babypic.jpg",
+            @"images/blue-angels.jpg",
+            @"images/blue-angels.jpg",
+            @"images/FancyBroodGum.jpg",
+            @"images/FancyBroodGum.jpg",
+            @"images/giraffe.jpg",
+            @"images/giraffe.jpg",
+            @"images/GirlAtGym.jpg",
+            @"images/GirlAtGym.jpg",
+            @"images/PipeSmoker.jpg",
+            @"images/PipeSmoker.jpg",
+            @"images/pragueNights.jpg",
+            @"images/pragueNights.jpg",
+            @"images/UglyFlower.jpg",
+            @"images/UglyFlower.jpg",
+            @"images/EasterIsland.jpg",
+            @"images/EasterIsland.jpg",
+            @"images/world-mask.jpg",
+            @"images/world-mask.jpg"
+        };
+        private static readonly string[] hardPairs = new string[]
+        {
+            @"images/babypic.jpg",
+            @"images/babypic.jpg",
+            @"images/blue-angels.jpg",
+            @"images/blue-angels.jpg",
+            @"images/FancyBroodGum.jpg",
+            @"images/FancyBroodGum.jpg",
+            @"images/giraffe.jpg",
+            @"images/giraffe.jpg",
+            @"images/GirlAtGym.jpg",
+            @"images/GirlAtGym.jpg",
+            @"images/PipeSmoker.jpg",
+            @"images/PipeSmoker.jpg",
+            @"images/pragueNights.jpg",
+            @"images/pragueNights.jpg",
+            @"images/UglyFlower.jpg",
+            @"images/UglyFlower.jpg",
+            @"images/EasterIsland.jpg",
+            @"images/EasterIsland.jpg",
+            @"images/world-mask.jpg",
+            @"images/world-mask.jpg",
+            @"images/Storm.jpg",
+            @"images/Storm.jpg",
+            @"images/shoppingCart.jpg",
+            @"images/shoppingCart.jpg",
+            @"images/SadClown.jpg",
+            @"images/SadClown.jpg",
+            @"images/rooster.jpg",
+            @"images/rooster.jpg",
+            @"images/rodent.jpg",
+            @"images/rodent.jpg",
+            @"images/CatFace.jpg",
+            @"images/CatFace.jpg",
+            @"images/Bread.jpg",
+            @"images/Bread.jpg",
+            @"images/Castle.jpg",
+            @"images/Castle.jpg"
+        };
     }
+
 }
